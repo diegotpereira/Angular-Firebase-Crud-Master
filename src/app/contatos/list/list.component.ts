0091a2be7093fea500122d0816304fefa442428c
Diego <diegoteixeirapereira@hotmail.com>
2021-02-18 09:33:53 -0300

    first commit

---
+++ src/app/contatos/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Contato } from './../shared/contato';
+import { ContatoService  } from '../shared/contato.service';
+import { Observable } from 'rxjs';
+import { ContatoDataService } from '../shared/contato-data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  contatos: Observable<any>;
+
+  constructor(private contatoService: ContatoService, private contatoDataService: ContatoDataService) { }
+
+  ngOnInit(): void {
+    this.contatos = this.contatoService.getAll();
+  }
+  delete(key: string){
+    this.contatoService.delete(key);
+  }
+  edit(contato: Contato, key: string){
+    this.contatoDataService.changeContato(contato, key);
+  }
+
+}
